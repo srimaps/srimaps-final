3be7eec9eba9034295afb2e55f94c44130cfe33d
Akith Nanayakkara <akith.20233080@iit.ac.lk>
2026-03-08 13:54:20 +0530

    Define UserContextType interface for global user state

--- frontend/src/context/UserContext.tsx
+++ frontend/src/context/UserContext.tsx
@@ -4,4 +4,14 @@ interface Driver {
   busNumber: string;
   name: string;
 }
+interface UserContextType {
+  role: UserRole;
+  setRole: (role: UserRole) => void;
+  driver: Driver | null;
+  loginDriver: (busNumber: string, password: string) => boolean;
+  logoutDriver: () => void;
+  isDriverSharingLocation: boolean;
+  setIsDriverSharingLocation: (sharing: boolean) => void;
+}
+
 
