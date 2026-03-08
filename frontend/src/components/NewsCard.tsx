d100d64aa9670078ef4cdeb64212e5f0f2a0fd1a
Dunal Senara <senaradunal@gmail.com>
2026-03-08 12:25:18 +0530

    Create NewsCard component with props interface

---
+++ frontend/src/components/NewsCard.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+import { NewspaperIcon, CalendarIcon, ClockIcon, BusIcon } from 'lucide-react';
+import { NewsItem } from '../utils/mockData';
+interface NewsCardProps {
+  news: NewsItem;
+  index: number;
+}
+export function NewsCard({ news, index }: NewsCardProps) {
+  return ();
+
+}
