c5adfee8a072c235f3122576bb79e0d10248a069
Sanvidu <sanvidu.20231566@iit.ac.lk>
2026-03-21 09:08:08 +0530

    uploading backend

---
+++ Backend/srimaps_backend/src/main/java/com/srimaps/backend/SrimapsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.srimaps.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrimapsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrimapsBackendApplication.class, args);
+	}
+
+}
---
+++ Backend/srimaps_backend/src/test/java/com/srimaps/backend/SrimapsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srimaps.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrimapsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
