ab1b77293741bae73c2dfbd5dc9263626919d18c
Sanvidu <sanvidu.20231566@iit.ac.lk>
2026-03-22 21:25:33 +0530

    Implement BusService class for bus operations

---
+++ Backend/src/main/java/com/srimaps/backend/service/BusService.java
@@ -0,0 +1,20 @@
+package com.srimaps.backend.service;
+
+import com.srimaps.backend.entity.Bus;
+import com.srimaps.backend.exception.ResourceNotFoundException;
+import com.srimaps.backend.repository.BusRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BusService {
+
+    private final BusRepository busRepository;
+
+    public BusService(BusRepository busRepository) {
+        this.busRepository = busRepository;
+    }
+
+    
+}
