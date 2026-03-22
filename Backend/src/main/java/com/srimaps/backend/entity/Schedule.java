ab8324f446bf487fcca5fd900ba875bea2f91a76
Gayan Nanayakkara <gsnanayakkara345@gmail.com>
2026-03-22 17:28:35 +0530

    Add Schedule entity class with JPA annotations

---
+++ Backend/src/main/java/com/srimaps/backend/entity/Schedule.java
@@ -0,0 +1,8 @@
+package com.srimaps.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalTime;
