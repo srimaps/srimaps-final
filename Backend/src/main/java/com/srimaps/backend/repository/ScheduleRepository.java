f7b9c9017a5db289fd6d1cbdf5e1a342d432ef6b
Sanvidu <sanvidu.20231566@iit.ac.lk>
2026-03-22 21:03:09 +0530

    Add method to fetch schedules ordered by departure time

--- Backend/src/main/java/com/srimaps/backend/repository/ScheduleRepository.java
+++ Backend/src/main/java/com/srimaps/backend/repository/ScheduleRepository.java
@@ -6,5 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ScheduleRepository extends JpaRepository<Schedule, Integer> {
+  
+    List<Schedule> findAllByOrderByDepartureTimeAsc();
+
+
 
 }
