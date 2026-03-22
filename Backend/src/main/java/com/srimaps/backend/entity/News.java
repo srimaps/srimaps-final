51968e0e28f82a03d166153533e46de80c287fbe
Sanvidu <sanvidu.20231566@iit.ac.lk>
2026-03-22 11:57:55 +0530

    Add News entity class for news table

---
+++ Backend/src/main/java/com/srimaps/backend/entity/News.java
@@ -0,0 +1,17 @@
+package com.srimaps.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "news")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class News {
+
+}
