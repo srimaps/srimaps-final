cb68c831f2ba5f1e9996d7e3882ab43b779d6bdb
Dunal Senara <senaradunal@gmail.com>
2026-03-22 11:22:16 +0530

    Refactor LoginRequest class with validation annotations

--- Backend/src/main/java/com/srimaps/backend/dto/LoginRequest.java
+++ Backend/src/main/java/com/srimaps/backend/dto/LoginRequest.java
@@ -2,3 +2,13 @@ package com.srimaps.backend.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
+
+@Data
+public class LoginRequest {
+
+    @NotBlank(message = "Username is required")
+    private String username;
+
+    @NotBlank(message = "Password is required")
+    private String password;
+}
