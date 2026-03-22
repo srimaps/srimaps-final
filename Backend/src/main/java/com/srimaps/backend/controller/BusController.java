d970ff4414e38420c3744d11146121e53c3db957
Akith Nanayakkara <akith.20233080@iit.ac.lk>
2026-03-22 10:17:38 +0530

    add BusController skeleton

---
+++ Backend/src/main/java/com/srimaps/backend/controller/BusController.java
@@ -0,0 +1,10 @@
+package com.srimaps.backend.controller;
+
+import com.srimaps.backend.entity.Bus;
+import com.srimaps.backend.service.BusService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+public class BusController {
+}
