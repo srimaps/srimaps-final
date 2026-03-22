ece81d194fa8bb5c8cc3a22dc72d7a41d0be6187
Sanvidu <sanvidu.20231566@iit.ac.lk>
2026-03-22 11:58:12 +0530

    Add fields to News entity for database mapping

--- Backend/src/main/java/com/srimaps/backend/entity/News.java
+++ Backend/src/main/java/com/srimaps/backend/entity/News.java
@@ -14,4 +14,24 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public class News {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "news_id")
+    private Integer newsId;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "description", nullable = false, columnDefinition = "TEXT")
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "route_id")
+    private Route route;
+
+    @Column(name = "posted_by")
+    private String postedBy = "Admin";
+
+    @Column(name = "posted_at")
+    private LocalDateTime postedAt = LocalDateTime.now();
 }
