da5b4eec0787799da3a8656b536e602fc4c0b684
Gayan Nanayakkara <gsnanayakkara345@gmail.com>
2026-03-22 18:04:26 +0530

    Refactor NewsRequest class with validation annotations

--- Backend/src/main/java/com/srimaps/backend/dto/NewsRequest.java
+++ Backend/src/main/java/com/srimaps/backend/dto/NewsRequest.java
@@ -2,3 +2,9 @@ package com.srimaps.backend.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
+
+@Data
+public class NewsRequest {
+
+    @NotBlank(message = "Title is required")
+    private String title;
