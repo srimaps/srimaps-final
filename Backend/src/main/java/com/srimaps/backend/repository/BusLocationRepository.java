27fbb9e0682cbaab0d0500e8dae83fae8be9c3ee
Akith Nanayakkara <akith.20233080@iit.ac.lk>
2026-03-22 21:14:00 +0530

    Update BusLocationRepository.java

---
+++ Backend/src/main/java/com/srimaps/backend/repository/BusLocationRepository.java
@@ -0,0 +1,8 @@
+package com.srimaps.backend.repository;
+
+import com.srimaps.backend.entity.BusLocation;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
