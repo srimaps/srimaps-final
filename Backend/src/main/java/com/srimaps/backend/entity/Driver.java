eeb8a05cb245d756bd1d003ae579e337c9115c8e
Dunal Senara <senaradunal@gmail.com>
2026-03-22 11:07:09 +0530

    Reformat Driver.java with proper annotations

--- Backend/src/main/java/com/srimaps/backend/entity/Driver.java
+++ Backend/src/main/java/com/srimaps/backend/entity/Driver.java
@@ -4,3 +4,9 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "drivers")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
