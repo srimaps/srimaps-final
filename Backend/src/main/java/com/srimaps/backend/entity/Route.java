9b7bc5a650b36cafc8f613defd06e8b6aefa6556
Sanvidu <sanvidu.20231566@iit.ac.lk>
2026-03-22 08:57:36 +0530

    route entity initialisation

---
+++ Backend/src/main/java/com/srimaps/backend/entity/Route.java
@@ -0,0 +1,16 @@
+package com.srimaps.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "routes")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Route {
+
+
+}
