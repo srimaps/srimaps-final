68ad8df4cfb70e2458b6cae6360dd27e6d68090e
Akith Nanayakkara <akith.20233080@iit.ac.lk>
2026-03-22 10:30:48 +0530

    add DriverController skeleton

---
+++ Backend/src/main/java/com/srimaps/backend/controller/DriverController.java
@@ -0,0 +1,11 @@
+package com.srimaps.backend.controller;
+
+import com.srimaps.backend.dto.LoginRequest;
+import com.srimaps.backend.dto.LoginResponse;
+import com.srimaps.backend.entity.Driver;
+import com.srimaps.backend.service.DriverService;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+
+public class DriverController {
+}
