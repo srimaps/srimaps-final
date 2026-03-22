32a42d33dd590bd6439c61ac9c761be48b2b2ae0
Gayan Nanayakkara <gsnanayakkara345@gmail.com>
2026-03-22 17:58:21 +0530

    Add ScheduleController with initial imports

---
+++ Backend/src/main/java/com/srimaps/backend/controller/ScheduleController.java
@@ -0,0 +1,5 @@
+package com.srimaps.backend.controller;
+
+import com.srimaps.backend.entity.Schedule;
+import com.srimaps.backend.service.ScheduleService;
+import org.springframework.web.bind.annotation.GetMapping;
