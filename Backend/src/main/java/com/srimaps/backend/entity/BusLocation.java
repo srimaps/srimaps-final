100870637366887a6c98fb1732d865dc40c1df15
Akith Nanayakkara <akith.20233080@iit.ac.lk>
2026-03-22 09:51:22 +0530

    add BusLocation entity class skeleton
    Created the BusLocation entity class with basic package structure and imports.

---
+++ Backend/src/main/java/com/srimaps/backend/entity/BusLocation.java
@@ -0,0 +1,11 @@
+package com.srimaps.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+public class BusLocation {
+}
