6ea5e42e0b8dc416be68645c7e84194cb6cda0ba
Akith Nanayakkara <akith.20233080@iit.ac.lk>
2026-03-22 10:43:33 +0530

    add fields to LostFoundRequest DTO

--- Backend/src/main/java/com/srimaps/backend/dto/LostFoundRequest.java
+++ Backend/src/main/java/com/srimaps/backend/dto/LostFoundRequest.java
@@ -6,4 +6,11 @@ import lombok.Data;
 
 @Data
 public class LostFoundRequest {
+  
+  private String itemType;
+  private String itemName;
+  private String description;
+  private String contactInfo;
+  private Integer routeId;
+
 }
