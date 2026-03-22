e97751825bdae5d74c4ece27edf02212790b26b1
Sanvidu <sanvidu.20231566@iit.ac.lk>
2026-03-22 20:35:58 +0530

    Add CorsConfig class

---
+++ Backend/src/main/java/com/srimaps/backend/config/CorsConfig.java
@@ -0,0 +1,7 @@
+package com.srimaps.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
