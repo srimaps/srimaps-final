e371b8b53d96f925e78e6a07be183aa28036f931
dhanith-bandara <dhanith.20233048@iit.ac.lk>
2026-03-22 17:20:20 +0530

    Add List import

--- Backend/src/main/java/com/srimaps/backend/controller/NewsController.java
+++ Backend/src/main/java/com/srimaps/backend/controller/NewsController.java
@@ -5,3 +5,4 @@ import com.srimaps.backend.entity.News;
 import com.srimaps.backend.service.NewsService;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
+import java.util.List;
