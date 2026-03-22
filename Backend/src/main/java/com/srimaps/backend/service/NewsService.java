d1f573e22b854cda1909a3241a52eaa85512c2d1
Dunal Senara <senaradunal@gmail.com>
2026-03-22 21:38:02 +0530

    Add NewsService class for news management

---
+++ Backend/src/main/java/com/srimaps/backend/service/NewsService.java
@@ -0,0 +1,11 @@
+package com.srimaps.backend.service;
+
+import com.srimaps.backend.dto.NewsRequest;
+import com.srimaps.backend.entity.News;
+import com.srimaps.backend.entity.Route;
+import com.srimaps.backend.repository.NewsRepository;
+import com.srimaps.backend.repository.RouteRepository;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
