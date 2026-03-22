fe08c902a024f6b5017835d252a6ae1809c64340
dhanith-bandara <dhanith.20233048@iit.ac.lk>
2026-03-22 17:18:48 +0530

    Add validation import

--- Backend/src/main/java/com/srimaps/backend/controller/NewsController.java
+++ Backend/src/main/java/com/srimaps/backend/controller/NewsController.java
@@ -3,3 +3,4 @@ package com.srimaps.backend.controller;
 import com.srimaps.backend.dto.NewsRequest;
 import com.srimaps.backend.entity.News;
 import com.srimaps.backend.service.NewsService;
+import jakarta.validation.Valid;
