8502739729920e942ae8a79a1a07050f5c26aa76
Sanvidu <sanvidu.20231566@iit.ac.lk>
2026-03-22 20:50:48 +0530

    Add methods to DriverRepository for user lookup

--- Backend/src/main/java/com/srimaps/backend/repository/DriverRepository.java
+++ Backend/src/main/java/com/srimaps/backend/repository/DriverRepository.java
@@ -5,3 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
+public interface DriverRepository extends JpaRepository<Driver, Integer> {
+
+    Optional<Driver> findByUsername(String username);
+
+    Optional<Driver> findByUsernameAndPasswordAndIsActiveTrue(String username, String password);
+}
