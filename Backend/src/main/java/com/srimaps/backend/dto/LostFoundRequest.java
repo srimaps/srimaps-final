1cd41cd7939c402acbe87b018d6b92fb736e6b23
Akith Nanayakkara <akith.20233080@iit.ac.lk>
2026-03-22 10:45:36 +0530

    add validation for itemType and itemName

--- Backend/src/main/java/com/srimaps/backend/dto/LostFoundRequest.java
+++ Backend/src/main/java/com/srimaps/backend/dto/LostFoundRequest.java
@@ -7,10 +7,13 @@ import lombok.Data;
 @Data
 public class LostFoundRequest {
   
+  @NotBlank(message = "Item type is required")
   private String itemType;
+
+  @NotBlank(message = "Item name is required")
   private String itemName;
   private String description;
   private String contactInfo;
   private Integer routeId;
 
-}
+} 
