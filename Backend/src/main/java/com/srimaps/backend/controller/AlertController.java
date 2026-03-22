e1ac2c45d96b6ab1b34ac568eae6ac9326639d72
Gayan Nanayakkara <gsnanayakkara345@gmail.com>
2026-03-22 17:53:21 +0530

    Reorganize imports in AlertController.java

--- Backend/src/main/java/com/srimaps/backend/controller/AlertController.java
+++ Backend/src/main/java/com/srimaps/backend/controller/AlertController.java
@@ -2,3 +2,7 @@ package com.srimaps.backend.controller;
 
 import com.srimaps.backend.entity.Alert;
 import com.srimaps.backend.service.AlertService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
