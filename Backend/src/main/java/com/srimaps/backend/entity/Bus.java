9441f75e942750dd42c9156fe5bf59016bbb7346
Akith Nanayakkara <akith.20233080@iit.ac.lk>
2026-03-22 10:02:06 +0530

    add Bus entity class skeleton

---
+++ Backend/src/main/java/com/srimaps/backend/entity/Bus.java
@@ -0,0 +1,9 @@
+package com.srimaps.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+public class Bus {
+}
