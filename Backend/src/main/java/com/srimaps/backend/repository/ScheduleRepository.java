98ac8bc3284f0f5fa30b49e0dbe0bb9615e60e66
Sanvidu <sanvidu.20231566@iit.ac.lk>
2026-03-22 21:02:42 +0530

    Create ScheduleRepository interface for Schedule entity

---
+++ Backend/src/main/java/com/srimaps/backend/repository/ScheduleRepository.java
@@ -0,0 +1,10 @@
+package com.srimaps.backend.repository;
+
+import com.srimaps.backend.entity.Schedule;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ScheduleRepository extends JpaRepository<Schedule, Integer> {
+
+}
