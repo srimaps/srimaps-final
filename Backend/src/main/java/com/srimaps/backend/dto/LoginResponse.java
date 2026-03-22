810ed274f73e0712c895ad8479d5809d10779128
Dunal Senara <senaradunal@gmail.com>
2026-03-22 11:36:00 +0530

    Refactor LoginResponse class structure

--- Backend/src/main/java/com/srimaps/backend/dto/LoginResponse.java
+++ Backend/src/main/java/com/srimaps/backend/dto/LoginResponse.java
@@ -7,3 +7,12 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+public class LoginResponse {
+    private Integer driverId;
+    private String fullName;
+    private String username;
+    private Integer busId;
+    private String busNumber;
+    private String routeNumber;
+    private String message;
+}
