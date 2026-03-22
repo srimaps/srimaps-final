aeea1c739f2e77d2459b1477e3205fb60cd9aac3
Sanvidu <sanvidu.20231566@iit.ac.lk>
2026-03-22 21:35:46 +0530

    Update LostFoundService.java

---
+++ Backend/src/main/java/com/srimaps/backend/service/LostFoundService.java
@@ -0,0 +1,16 @@
+import com.srimaps.backend.entity.Route;
+import com.srimaps.backend.repository.LostFoundRepository;
+import com.srimaps.backend.repository.RouteRepository;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+public class LostFoundService {
+
+    private final LostFoundRepository lostFoundRepository;
+    private final RouteRepository routeRepository;
+
+
+}
