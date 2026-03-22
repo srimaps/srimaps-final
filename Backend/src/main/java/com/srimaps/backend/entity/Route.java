7efa6d611a31a4af39461dd5ec97832f347d8ab9
Sanvidu <sanvidu.20231566@iit.ac.lk>
2026-03-22 08:58:00 +0530

    Add fields to Route entity class

--- Backend/src/main/java/com/srimaps/backend/entity/Route.java
+++ Backend/src/main/java/com/srimaps/backend/entity/Route.java
@@ -11,6 +11,25 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Route {
+      @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "route_id")
+    private Integer routeId;
+
+    @Column(name = "route_number", nullable = false, unique = true)
+    private String routeNumber;
+
+    @Column(name = "start_destination", nullable = false)
+    private String startDestination;
+
+    @Column(name = "end_destination", nullable = false)
+    private String endDestination;
+
+    @Column(name = "distance_km")
+    private Double distanceKm;
+
+    @Column(name = "is_active")
+    private Boolean isActive = true;
 
 
 }
