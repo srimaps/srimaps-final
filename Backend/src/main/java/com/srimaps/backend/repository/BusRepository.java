e335517c8364777dd4832e06970c0761048a0387
Akith Nanayakkara <akith.20233080@iit.ac.lk>
2026-03-22 21:13:28 +0530

    Update BusRepository.java

--- Backend/src/main/java/com/srimaps/backend/repository/BusRepository.java
+++ Backend/src/main/java/com/srimaps/backend/repository/BusRepository.java
@@ -5,3 +5,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
+public interface BusRepository extends JpaRepository<Bus, Integer> {
+
+    List<Bus> findByRoute_RouteNumber(String routeNumber);
+
+    List<Bus> findByRoute_StartDestinationIgnoreCaseAndRoute_EndDestinationIgnoreCase(
+            String startDestination,
+            String endDestination
+    );
+
+    List<Bus> findByIsSharingLocationTrue();
+}
+
