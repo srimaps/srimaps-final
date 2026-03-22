140844df28fdacc634e7cada59b737bb78477f3b
Dunal Senara <senaradunal@gmail.com>
2026-03-22 21:40:41 +0530

    Add ScheduleService class for schedule management

---
+++ Backend/src/main/java/com/srimaps/backend/service/ScheduleService.java
@@ -0,0 +1,7 @@
+package com.srimaps.backend.service;
+
+import com.srimaps.backend.entity.Schedule;
+import com.srimaps.backend.repository.ScheduleRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
