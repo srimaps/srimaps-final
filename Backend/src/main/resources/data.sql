2c905a0ba73f599541e58ef2fc5bb5ad6664fe18
Shyan Wijesekera <87508593+shyanraphael@users.noreply.github.com>
2026-03-23 07:03:38 +0530

    Inserting values into the bues

--- Backend/src/main/resources/data.sql
+++ Backend/src/main/resources/data.sql
@@ -3,3 +3,9 @@ INSERT INTO routes (route_id, route_number, start_destination, end_destination, 
 (2, '177', 'Colombo Fort', 'Kaduwela', 16.0, TRUE),
 (3, '120', 'Colombo Fort', 'Piliyandala', 20.0, TRUE)
 ON DUPLICATE KEY UPDATE route_number=VALUES(route_number);
+
+INSERT INTO buses (bus_id, bus_number, plate_number, route_id, capacity, status, is_sharing_location) VALUES
+(1, '138', 'NB-1381', 1, 54, 'ACTIVE', TRUE),
+(2, '177', 'NB-1773', 2, 54, 'ACTIVE', TRUE),
+(3, '120', 'NB-1202', 3, 54, 'ACTIVE', FALSE)
+ON DUPLICATE KEY UPDATE bus_number=VALUES(bus_number);
