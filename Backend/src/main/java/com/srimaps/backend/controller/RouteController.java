5aa9b78b4bab0ec4f9975f879e704b4c7a880897
Akith Nanayakkara <akith.20233080@iit.ac.lk>
2026-03-30 00:23:44 +0530

    Update RouteController.java

--- Backend/src/main/java/com/srimaps/backend/controller/RouteController.java
+++ Backend/src/main/java/com/srimaps/backend/controller/RouteController.java
@@ -1,1 +1,10 @@
+package com.srimaps.backend.controller;
 
+import com.srimaps.backend.entity.Route;
+import com.srimaps.backend.repository.RouteRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+public class RouteController {
+}
