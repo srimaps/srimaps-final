37e2691765a6f3d0789c95f957036bbe4e4477f5
Akith Nanayakkara <akith.20233080@iit.ac.lk>
2026-03-30 00:24:54 +0530

    Update RouteController.java

--- Backend/src/main/java/com/srimaps/backend/controller/RouteController.java
+++ Backend/src/main/java/com/srimaps/backend/controller/RouteController.java
@@ -6,5 +6,20 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@RestController
+@RequestMapping("/api/routes")
+@CrossOrigin(origins = "*")
 public class RouteController {
+  private final RouteRepository routeRepository;
+
+    public RouteController(RouteRepository routeRepository) {
+        this.routeRepository = routeRepository;
+    }
+
+    @GetMapping
+    public List<Route> getAllRoutes() {
+        return routeRepository.findAll();
+    }
 }
+
+
